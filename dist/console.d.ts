02b96cf7f32d32a7b18bcb5a1b4d8d4d0ed71540
BertHooger <berthooger1@gmail.com>
2025-06-30 11:51:01 -0400

    Wait production is better to keep node_modules

---
+++ dist/console.d.ts
@@ -0,0 +1,14 @@
+import { DutchName } from './generator';
+export declare class PrettyConsole {
+    private generator;
+    constructor();
+    showTitle(): Promise<void>;
+    showWelcomeBox(): void;
+    showSingleName(title: string, name: DutchName): void;
+    showNameTable(title: string, names: DutchName[]): void;
+    showStats(names: DutchName[]): void;
+    showUsageExamples(): void;
+    generateWithSpinner(count: number, description: string): Promise<DutchName[]>;
+    showFooter(): void;
+}
+//# sourceMappingURL=console.d.ts.map
---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { DutchPhoneticNameGenerator } from './generator';
+export { DutchPhoneticNameGenerator, DutchName } from './generator';
+export declare const dutchNameGenerator: DutchPhoneticNameGenerator;
+//# sourceMappingURL=index.d.ts.map
